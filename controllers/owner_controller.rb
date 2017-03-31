583da0377faa904a2feab62c8710e375bb885144
Euan Fullarton <euanfullarton7@gmail.com>
2017-03-31 15:45:14 +0100

    basic add owner method working

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -6,3 +6,14 @@ get '/owners' do
   @owners = Owner.all()
   erb ( :"owners/index" )
 end
+
+get '/owners/new' do
+  @owners = Owner.all()
+  erb(:"owners/new")
+end
+
+post '/owners' do
+  @owner = Owner.new(params)
+  @owner.save()
+  erb(:"owners/create")
+end
