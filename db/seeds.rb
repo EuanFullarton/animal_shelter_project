d03f52009ffbb13167fe5eb25b4dc973945c30cd
Euan Fullarton <euanfullarton7@gmail.com>
2017-04-01 16:31:22 +0100

    Amended new animal form to include some dropdown menus. Not yet unique values

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,35 +18,25 @@ owner1 = Owner.new({
 
 owner2 = Owner.new({
   'name' => "Percy",
-  'animals_owned' => 'Jerry',
-  'bio' => 'Percy lives in a studio flat so would like a small animal',
+  'animals_owned' => 'Clarence',
+  'bio' => 'Percy loves cats',
   'capacity_for_animals' => 2,
-  'looking_for' => 'Rat',
+  'looking_for' => 'Cat',
   'profile_picture' => 'http://pix.iemoji.com/images/emoji/apple/ios-9/256/white-older-man.png'
 })
 
 owner3 = Owner.new({
   'name' => "Jemima",
-  'animals_owned' => 'Chick',
+  'animals_owned' => 'Doug',
   'bio' => 'Jemima owns a farm and is looking for several animals',
-  'capacity_for_animals' => 10,
-  'looking_for' => 'Dog' 'Cat' 'Turtle' 'Chicken',
+  'capacity_for_animals' => 6,
+  'looking_for' => 'Dog' 'Cat',
   'profile_picture' => 'https://thumbs.dreamstime.com/x/female-farmer-holding-pitchfork-bucket-20260568.jpg'
 })
-
-# owner4 = Owner.new({
-#   'name' => "Euan",
-#   'animals_owned' => 'None',
-#   'bio' => 'Euan is a student who has always wanted a dog',
-#   'capacity_for_animals' => 1,
-#   'looking_for' => 'Dog',
-#   'profile_picture' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Stick_Figure.svg/170px-Stick_Figure.svg.png'
-# })
 
 owner1.save
 owner2.save
 owner3.save
-# owner4.save
 
 animal1 = Animal.new({
   'name' => "Rufus",
@@ -61,45 +51,45 @@ animal1 = Animal.new({
 })
 
 animal2 = Animal.new({
-  'name' => "Jerry",
-  'type' => 'Mouse',
-  'breed' => 'House Mouse',
-  'bio' => 'Jerry is a crafty mouse',
+  'name' => "Clarence",
+  'type' => 'Cat',
+  'breed' => 'Tom',
+  'bio' => 'Clarence is a lazy cat',
   'ready_for_adoption' => false,
   'adopted' => true,
   'adoption_date' => '02/01/2016',
   'admission_date' => '03/12/2015',
-  'profile_picture' => 'https://static.giantbomb.com/uploads/scale_small/0/5768/538236-jerry.jpg'
+  'profile_picture' => 'https://pbs.twimg.com/profile_images/710983778802409472/EdOHlXdK.jpg'
 })
 
 animal3 = Animal.new({
-  'name' => "Chick",
-  'type' => 'Chicken',
-  'breed' => 'Leghorn',
-  'bio' => 'Chick is a loud chicken',
+  'name' => "Doug",
+  'type' => 'Dog',
+  'breed' => 'Pug',
+  'bio' => 'Doug is a pug',
   'ready_for_adoption' => false,
   'adopted' => true,
   'adoption_date' => '03/10/2014',
   'admission_date' => '13/08/2014',
-  'profile_picture' => 'https://s-media-cache-ak0.pinimg.com/originals/1a/b9/0f/1ab90fd9318000adfa57a81fc2b1c656.jpg'
+  'profile_picture' => 'https://pbs.twimg.com/profile_images/662398361971105792/SOGHt_tL.jpg'
 })
 
-# animal4 = Animal.new({
-#   'name' => "Rufus",
-#   'type' => 'Dog',
-#   'breed' => 'Labrador',
-#   'bio' => 'Rufus is a friendly labrador',
-#   'ready_for_adoption' => true,
-#   'adopted' => false,
-#   'adoption_date' => 'N/A',
-#   'admission_date' => '12/02/2017',
-#   'profile_picture' => 'https://static.pexels.com/photos/7720/night-animal-dog-pet.jpg'
-# })
+animal4 = Animal.new({
+  'name' => "Pierre",
+  'type' => 'Cat',
+  'breed' => 'Siamese',
+  'bio' => 'Pierre is a Siamese cat of unclear origin',
+  'ready_for_adoption' => true,
+  'adopted' => false,
+  'adoption_date' => 'N/A',
+  'admission_date' => '12/02/2017',
+  'profile_picture' => 'http://siamesebreedersgroup.weebly.com/uploads/5/2/7/0/5270013/8451140.jpg?256'
+})
 
 animal1.save
 animal2.save
 animal3.save
-# animal4.save
+animal4.save
 
 adoption1 = Adoption.new({
   'animal_id' => animal2.id,
